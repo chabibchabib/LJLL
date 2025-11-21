f93bffb672b0f1666f26b8e85f534d126fb67a05
ahmed chabib <ahmed@pl787-pro.paris.inria.fr>
2025-11-21 12:22:27 +0100

    Aajout de la fct d'intersection

--- Raycasting/rayreflect.cpp
+++ Raycasting/rayreflect.cpp
@@ -84,10 +84,10 @@ struct rectangle{
         vector<real> P3 = { P1[0] + (P2[0]-P0[0]),
                             P1[1] + (P2[1]-P0[1]) };
         real eps = 1e-12;
-        if(pointonsegment(P,P0,P1,eps)) {A=P0; B=P1;}
-        if(pointonsegment(P,P1,P3,eps)) {A=P1; B=P3;}
-        if(pointonsegment(P,P3,P2,eps)) {A=P3; B=P2;}
-        if(pointonsegment(P,P2,P0,eps)) {A=P2; B=P0;}
+        if(pointonsegment(P,P0,P1,eps)) {A=P1; B=P0;}
+        if(pointonsegment(P,P1,P3,eps)) {A=P3; B=P1;}
+        if(pointonsegment(P,P3,P2,eps)) {A=P2; B=P3;}
+        if(pointonsegment(P,P2,P0,eps)) {A=P0; B=P2;}
 
         real dx = B[0] - A[0];
         real dy = B[1] - A[1];
@@ -99,21 +99,68 @@ struct rectangle{
         return normal;
     }
 
+    // calcul le nouveau faisceau
+    // reflected par une surface 
+    //template<int dim>
+    ray<dim> reflection(ray<dim> r, vector<real> intersecpoint ){
+        vector<real> normal= getnormal(intersecpoint);
+        ray<dim> res;
+        real ddotn=0.;
+        res.origin= intersecpoint;
+        for (int i=0; i<dim; i++) ddotn+=normal[i]*r.dir[i];
+        res.dir=r.dir;
+        for (int i=0; i<dim; i++) res.dir[i]-=2*ddotn*normal[i];
+
+        return res ; 
+    }
+
+    // PA et PB forment un EDGE du polygone
+    // equa segement PA+s*(PB-PA) et celle de ray O+td
+    vector<real> intessectionpoint(ray<dim> r, vector<real> PA,vector<real> PB ){
+        vector<real> point(dim);
+        //Crammer's rule
+        real ABx = (PB[0]-PA[0]), ABy= (PB[1]-PA[1]), AOx=(r.origin[0]-PA[0]),AOy=(r.origin[1]-PA[1]);
+
+        real det=-ABx*r.dir[1] + ABy*r.dir[0];
+        assert(abs(det)> 1e-12);
+        det=1/det;
+        real s= det*(r.dir[0]*AOy - r.dir[1]*AOx );
+        cout<<"s="<<s<<"det="<< 1/det<<endl;
+        assert(s>=0 && s<=1); // appartient au segment?
+        real t= det*(ABx*AOy - ABy*AOx );
+        assert(t>=1e-12); // appartient au faisceau?
+
+        point[0]= r.origin[0]+ t*r.dir[0];
+        point[1]= r.origin[1]+ t*r.dir[1];
+        cout<<"Test ="<<PA[0]+s*(PB[0]-PA[0])<<","<<PA[1]+s*(PB[1]-PA[1])<<endl;
+        return point;
+    }
+
     rectangle(vector<real> P00, vector<real> P11, vector<real> P22) :  P0(P00),P1(P11),P2(P22) {}
 }  ;
 
+void coutpoint(string message, vector<real> pt){
+    cout<<message+"= "<<pt[0]<<" , "<<pt[1]<<endl;
+}
 int main(){
     ray<2> r;
 
-    /*r.origin={0,0};
-    r.dir={1,2};*/
+    r.origin={-1,1};
+    r.dir={1,1};
     vector<real> P0={0,0};
-    vector<real> P1={1,0};
+    vector<real> P1={3,0};
     vector<real> P2={0,3};
     rectangle<2> Rec(P0,P1,P2);
     Rec.computeDim();
     cout<<"HxW= "<<Rec.height<<" , "<<Rec.width<<endl;
-    vector<real> n=Rec.getnormal({1,2});
-    cout<<"normal= "<<n[0]<<" , "<<n[1]<<endl;
+    vector<real> ptest={3,1};
+    vector<real> n=Rec.getnormal(ptest);
+    coutpoint("normal", n);
+    vector<real> pt=Rec.intessectionpoint(r, Rec.P0,Rec.P2 );
+    coutpoint("Intersection pt", pt);
+    coutpoint("P0", Rec.P0);
+    coutpoint("P1", Rec.P1);
+    coutpoint("P2", Rec.P2);
+
     return 0;
 }
