b49c8ad67175c22eb80aad48b2cd5aa35a75f16b
ahmed chabib <ahmed.chabib16@gmail.com>
2025-12-05 13:56:39 +0100

    Added plot

---
+++ FreeFemDays/cpp_codes/test.cpp
@@ -0,0 +1,24 @@
+void BasisFctPK(int , vector<vector<long>> &lambdas, 
+        vector<vector<long>> &shift, 
+        vector<long> &ff) {
+    int idx = 0;
+    for (auto &coordinate : coordinate_list) {
+        int i = coordinate[0];
+        int j = coordinate[1];
+        int k = coordinate[2];
+        if (i + j + k == p) {
+            int ID = 0;
+            ff[idx] = factorial(i)*factorial(j)
+                    *factorial(k);
+            if (i > 0) {
+                for (int ii = 0; ii < i ; ii++) {
+                    lambdas[idx][ID] = 0;
+                    shift[idx][ID] = ii;
+                    ID++;
+                }
+            }
+            // same for j and k
+            idx++;
+            }
+        }
+    }
